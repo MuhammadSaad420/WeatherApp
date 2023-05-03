eb4cdac1063dc5215dd2dbaaf484d52b51629f79
Fakhar <fakhar.oraxtech@gmail.com>
2023-05-03 12:40:55 +0500

    Preparing permission request using Dexter

--- app/src/main/java/com/example/weathermap/MainActivity.kt
+++ app/src/main/java/com/example/weathermap/MainActivity.kt
@@ -1,24 +1,83 @@
 package com.example.weathermap
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         if(isLocationEnabled()) {
-            Toast.makeText(this,"Your location is enabled", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(object: MultiplePermissionsListener {
+                    override fun onPermissionsChecked(result: MultiplePermissionsReport?) {
+                        if(result!!.areAllPermissionsGranted()) {
+                            getUserCurrentLocation()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        showActivatePermissionDialogPrompt()
+                    }
+
+                }).onSameThread().check()
         } else {
             Toast.makeText(this,"Your location is not enabled", Toast.LENGTH_SHORT).show()
             startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserCurrentLocation() {
+        val locationRequest = LocationRequest()
+        locationRequest.interval = 2000
+        locationRequest.fastestInterval = 1000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        fusedLocationClient.requestLocationUpdates(locationRequest, object: LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                val location = locationResult.lastLocation;
+            }
+   }, null)
+    }
+
+    private fun showActivatePermissionDialogPrompt() {
+        AlertDialog.Builder(this).setTitle("Location Permission!!")
+            .setPositiveButton("go to settings", DialogInterface.OnClickListener { _, _ ->
+
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                val uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, _ ->
+                dialog.dismiss()
+            })
+            .create()
+            .show()
+    }
+
     private fun isLocationEnabled():Boolean {
         val lm: LocationManager =
             applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager;
